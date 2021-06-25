7c0da45ad25633c7aa80e91faf45d16710d19d46
Pashalis Grivas <43899619+p17griv@users.noreply.github.com>
2021-06-25 17:08:02 +0300

    Update footer.php

--- beehive-control-web/footer.php
+++ beehive-control-web/footer.php
@@ -5,7 +5,7 @@ echo '
         <div class="text-center py-3">
             <p>A project of <br><a target="_blank" style="color: #dbb961;" href="https://github.com/p17griv">Pashalis Grivas</a>, <a target="_blank" style="color: #dbb961;" href="https://github.com/p17gera">George Gerarchakis</a> & <a target="_blank" style="color: #dbb961;" href="https://github.com/p17meli">George Melidoniotis</a></p>
             <span class="copyright"> Beehive Control | Copyright ©<script>document.write(new Date().getFullYear());</script></span>
-            <a href="#" target="_blank">
+            <a href="https://github.com/p17griv/beehive-control" target="_blank">
                 <i><img style="width: 25px; height: 25px" src="https://image.flaticon.com/icons/png/512/25/25231.png" alt="Github icon"></i>
             </a>
         </div>
